67e21518eca1d9cb0caf2e1a5155ed72151b31eb
thanhdc <thanhdc@ql6625.fr>
2020-06-10 14:47:31 +0700

    Janus basic setup and type def

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Janus from '../utils/janus';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  async ngOnInit() {
+    await Janus.initPromise({ debug: 'all' })
+
+    // const session = new Janus({
+    const session = await Janus.newPromise({
+      server: 'http://localhost:8088/janus',
+      destroyed: () => console.log('destroyed')
+    })
+
+    // ;(Janus as any).listDevices(x => console.log(x))
+    const handle = await session.attachPromise({
+      plugin: "janus.plugin.videoroom",
+      consentDialog: (on) => console.log('consentDialog', on),
+      webrtcState: (active, reason) => console.log('webrtcState', active, reason),
+      iceState: (state) => console.log('iceState', state),
+      mediaState: (type, on) => console.log('mediaState', type, on),
+      slowLink: (uplink, lost) => console.log('slowLink', uplink, lost),
+      onmessage: (msg, jsep) => console.log('onmessage', msg, jsep),
+      onlocalstream: (stream) => console.log('onlocalstream', stream),
+      onremotestream: (stream) => console.log('onremotestream', stream),
+      ondataopen: (label) => console.log('ondataopen', label),
+      ondata: (data, label) => console.log('ondata', data, label),
+      oncleanup: () => console.log('oncleanup'),
+      ondetached: () => console.log('ondetached')
+    })
+  }
 
 }
